49338bec046862528d91c190ec4ac8300ed481e6
June Park <junepark202012@gmail.com>
2024-04-03 07:36:08 +0900

    add wrangler, log to /greet endpoint

--- src/pages/greet.ts
+++ src/pages/greet.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = () => {
+    console.log("Shouldn't this be logged?")
     return new Response(
         JSON.stringify({
             greeting: 'Hello',
